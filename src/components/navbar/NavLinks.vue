7eccd73f621e2137d6cadd65163e8ba208502a12
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-20 20:14:27 +0300

    add NavLinks component with navigation links

---
+++ src/components/navbar/NavLinks.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const links = [
+  { name: 'Home', to: '/' },
+  { name: 'Blog', to: '/blog' },
+  { name: 'About', to: '/about' },
+  { name: 'Contact', to: '/contact' },
+  { name: 'Pricing', to: '/price' }
+]
+</script>
