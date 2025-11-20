ed4ecc49dcb729f3b0b741fddde74b802ab3633f
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-20 20:47:08 +0300

    add SearchBar component for search functionality

---
+++ src/components/navbar/SearchBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+const emit = defineEmits<{ (e: 'search', query: string): void }>()
+
+const emitSearch = () => {
+  emit('search', searchQuery.value)
+}
+</script>
