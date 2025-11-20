859c105d63c947f0a1996257596ea10bb9be27ca
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-20 21:11:13 +0300

    add ProfileDropdown component for user profile management

---
+++ src/components/navbar/ProfileDropdown.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useClerk } from '@clerk/vue'
+
+const props = defineProps<{ user: any }>()
+console.log(props)
+const isDropdownOpen = ref(false)
+const toggleDropdown = () => (isDropdownOpen.value = !isDropdownOpen.value)
+
+const clerk = useClerk()
+const signOutUser = () => {
+  clerk.value?.signOut()
+  isDropdownOpen.value = false
+}
+</script>
