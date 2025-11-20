8d503aeb03a1005d19add029dbf5492c8900b42d
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-20 21:34:20 +0300

    add Navbar component with user authentication and search functionality

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useUser, SignInButton } from '@clerk/vue'
+import SearchBar from './SearchBar.vue'
+import NavLinks from './NavLinks.vue'
+import ProfileDropdown from './ProfileDropdown.vue'
+
+const { user } = useUser()
+
+const onSearch = (query: string) => {
+  console.log('Search query:', query)
+  // Later, you can emit this to filter blog posts or trigger router navigation
+}
+</script>
