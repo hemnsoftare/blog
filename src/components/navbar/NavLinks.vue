4747ef655a35be3617e5697e812c05936bd2b97c
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-20 20:25:34 +0300

    add template structure to NavLinks component

--- src/components/navbar/NavLinks.vue
+++ src/components/navbar/NavLinks.vue
@@ -1,3 +1,17 @@
+<template>
+  <ul class="flex items-center space-x-6">
+    <li v-for="link in links" :key="link.to">
+      <router-link
+        :to="link.to"
+        class="text-gray-600 hover:text-blue-500 transition-colors duration-300"
+        active-class="text-blue-500 font-medium"
+      >
+        {{ link.name }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
 <script setup lang="ts">
 const links = [
   { name: 'Home', to: '/' },
