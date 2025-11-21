b96c6d2c67b8baf1a4ab2397f6c3ce9688039a91
Zeravan Qadir <ensw06522@student.su.edu.krd.com>
2025-11-21 14:43:12 +0300

    add SEC_tow_item component template and image handling

--- feature-modules/information/components/about/SEC_tow_item.vue
+++ feature-modules/information/components/about/SEC_tow_item.vue
@@ -1,3 +1,29 @@
+<template>
+  <div :class="['flex items-center gap-6', reverse ? 'flex-row-reverse' : '']">
+    
+    <!-- Text -->
+    <div class="w-1/2">
+      <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
+      <p class="text-gray-600 w-1/2">{{ text }}</p>
+    </div>
+
+    <!-- Image -->
+    <div class="w-1/2">
+      <img
+        v-if="image"
+        :src="image"
+        :alt="title"
+        class="w-full h-44 object-cover rounded-xl shadow"
+      />
+
+      <div
+        v-else
+        class="w-full h-32 bg-blue-100 rounded-xl"
+      ></div>
+    </div>
+
+  </div>
+</template>
 <script setup lang="ts">
 defineProps<{
   title: string
