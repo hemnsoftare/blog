369c5b924330c3fba72b9904985645c08615baeb
sarbastsoft1-coder <sarbastsoft1@gmail.com>
2025-11-24 03:16:02 +0300

    Enhanced Update Blog view with improved layout and styling

--- src/feature-modules/create-blog/UpdateBlog.vue
+++ src/feature-modules/create-blog/UpdateBlog.vue
@@ -1,3 +1,10 @@
 <template>
-     <h1 class="text-red-300">Update Blog</h1>
+  <div>
+    <h1 class="text-red-300">Update Blog</h1>
+
+    <!-- New update blog section -->
+    <p class="text-gray-600 mt-2">
+      Here you can update your blog content.
+    </p>
+  </div>
 </template>
