792e0ba532c28ec255348976dd12a1dde931a457
sarbastsoft1-coder <sarbastsoft1@gmail.com>
2025-11-24 03:44:12 +0300

    add index.ts to export all authentication components for easier imports

--- src/feature-modules/auth/component/AuthCard.vue
+++ src/feature-modules/auth/component/AuthCard.vue
@@ -134,3 +134,4 @@ withDefaults(defineProps<Props>(), {
 }
 </style>
 
+
---
+++ src/feature-modules/auth/component/index.ts
@@ -0,0 +1,9 @@
+// Export all auth components for easier imports
+export { default as FormInput } from './FormInput.vue'
+export { default as FormButton } from './FormButton.vue'
+export { default as ErrorMessage } from './ErrorMessage.vue'
+export { default as PasswordStrength } from './PasswordStrength.vue'
+export { default as SocialAuth } from './SocialAuth.vue'
+export { default as AuthCard } from './AuthCard.vue'
+export { default as LoadingSpinner } from './LoadingSpinner.vue'
+
